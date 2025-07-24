08143f1900e7df0891366cfe33b36dc99a7c7068
samnodier <sam@samnodier.com>
2025-07-24 06:19:59 -0400

    Few more tables

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -11,29 +11,29 @@ const Announcements = () => {
           <div className="flex items-center justify-between">
             <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
             <span className="text-sm text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
-              <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, nulla perspiciatis.</p>
           </div>
+              <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, nulla perspiciatis.</p>
         </div>
         <div className="bg-mapPurpleLight rounded-md p-4">
           <div className="flex items-center justify-between">
             <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
             <span className="text-sm text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
-              <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, cum, quaerat!</p>
           </div>
+              <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, cum, quaerat!</p>
         </div>
         <div className="bg-mapYellowLight rounded-md p-4">
           <div className="flex items-center justify-between">
             <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
             <span className="text-sm text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
-              <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, cumque, quam.</p>
           </div>
+              <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, cumque, quam.</p>
         </div>
         <div className="bg-mapSkyLight rounded-md p-4">
           <div className="flex items-center justify-between">
             <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
             <span className="text-sm text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
-              <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, ipsam natus.</p>
           </div>
+              <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, ipsam natus.</p>
         </div>
         </div>
       </div>
